70e0e0dedcfb8e850f82669ae51fb8883b405fca
riyan2020 <68267240+riyan2020@users.noreply.github.com>
2022-09-07 00:15:09 +0530

    Add files via upload

---
+++ Patterns/second.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i = 0, j=0;
+    for (i= 0; i<5; i++){
+        for( j=0; j<i+1; j++ ){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
